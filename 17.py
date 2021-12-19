07d88188307f673028b6a778e09433da4f22b31b
Eremurus <doratochi0806@ezweb.ne.jp>
2021-12-19 21:20:35 +0900

    コメント入れた

--- 17.py
+++ 17.py
@@ -4,7 +4,10 @@ import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 
+#忘却係数
 gamma = 0.99
+
+#実際のy を生成する関数
 def y_k_real(k):
     belnui = np.random.randint(0,2)
     if belnui == 0:
@@ -13,12 +16,14 @@ def y_k_real(k):
         w = 1.0
     return np.sin(0.0001 * k) + w
 
+#忘却最小二乗法の関数
 def boukyaku_saisyou_nijou(Phi, theta, y):
     K = Phi / (1.0 + Phi)
     theta = theta + K * (y - theta)
     Phi = Phi / gamma - K * Phi / gamma
     return theta, Phi
 
+#パラメータ
 theta = 0.0
 epsilon = 10 ** (-6)
 Phi = 1.0 / epsilon
@@ -26,6 +31,7 @@ theta_list = []
 k_list = []
 sink_list = []
 
+#それぞれのk に対して予測値を計算
 for k in range(1, 10001):
     k_list.append(k)
     y = y_k_real(k)
@@ -33,6 +39,7 @@ for k in range(1, 10001):
     theta_list.append(theta)
     sink_list.append(np.sin(k * 0.0001))
 
+#プロット
 fig, ax = plt.subplots(facecolor="w")
 ax.plot(k_list, theta_list, label="estimate")
 ax.plot(k_list, sink_list, label="real theta")
